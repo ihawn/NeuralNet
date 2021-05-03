6146833bafc925e85bf6dd9de163e080766f68cb
ihawn <ihawn.pl15@gmail.com>
2021-05-03 10:58:21 -0500

    -

--- TitanicSurvival.py
+++ TitanicSurvival.py
@@ -13,4 +13,44 @@ dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.cs
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
-print(dftrain.head())
+#separate catagorical data
+catagorical_columns = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+numeric_columns = ['age', 'fare']
+
+#encode catagorical and numeric data
+feature_columns = []
+for feature_name in catagorical_columns:
+    vocabulary = dftrain[feature_name].unique() #gets a list of all unique values for any given feature column
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in numeric_columns:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+#define batches
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function()
+
+train_input_fn = lambda: make_input_fn(dftrain, y_train)
+eval_input_fn = lambda: make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+#train
+linear_est.train(train_input_fn)
+result = linear_est.evaluate(eval_input_fn)
+
+
+clear_output()
+
+#predict results
+result = list(linear_est.predict(eval_input_fn))
+print(dfeval.loc[3])
+print(y_eval.loc[3])
+print(result[3]['probabilities'][1])
