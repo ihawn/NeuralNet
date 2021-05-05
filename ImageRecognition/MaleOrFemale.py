d798f8a04e12f4e861119046f214eca9274e612a
ihawn <ihawn.pl15@gmail.com>
2021-05-05 18:10:20 -0500

    -

--- ImageRecognition/MaleOrFemale.py
+++ ImageRecognition/MaleOrFemale.py
@@ -12,11 +12,19 @@ from keras.datasets import cifar10
 import scipy.misc
 from PIL import Image
 from numpy import asarray
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+import random
 
 #paramaters
+seed = 10
 batch_size = 32
 img_height = 200
 img_width = 200
+split = 0.8
 
 #inialize face images into list
 path = pathlib.Path(r'C:\Users\Isaac\Documents\Datasets')
@@ -50,17 +58,90 @@ for i in range(0, len(faces)):
 #convert images to matrixes
 face_matrices = []
 
-#print("Convertinging images to matrices. This will probably take a minute or so...")
-#for i in range(0, len(faces)):
-#    face = Image.open(faces[i])
-#    face_matrices.append(asarray(face))
+print("Convertinging images to matrices. This will probably take a minute or so...")
+for i in range(0, len(faces)): #len(faces)
+    face = Image.open(faces[i])
+    face_matrices.append(asarray(face))
 
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-print(y_test)
-print(sex)
+#randomize the input and output arrays
+temp = list(zip(face_matrices, sex))
+random.shuffle(temp)
+face_matrices, sex = zip(*temp)
+
+#define x and y data and split it into train and test
+pivot = int(split*len(face_matrices))
+(x_train, y_train), (x_test, y_test) = (face_matrices[0:pivot], sex[0:pivot]), (face_matrices[pivot+1:len(face_matrices)], sex[pivot+1:len(face_matrices)])
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+#scale matrix data
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255.0
+x_test /= 255.0
+
+#one hot encode
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+class_num = y_test.shape[1]
+
+
+#model
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same')) #increase convolution filter size so the model can recognize more complex patterns
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(256, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(128, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(class_num))
+model.add(Activation('softmax'))
+
+#train
+epochs = 30
+optimizer = 'adam'
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+np.random.seed(seed)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=64)
+
+#test
+scores = model.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
 #plt.grid(False)
-#plt.imshow(face_matrices[120], cmap=plt.cm.binary)
+#plt.imshow(face_matrices[300], cmap=plt.cm.binary)
 #plt.show()
 
