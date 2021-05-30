463e4cc6d94777ce04df4af3395ae4ecfee20fdf
ihawn <ihawn.pl15@gmail.com>
2021-05-30 09:43:46 -0500

    -

--- ImageRecognition/image_generator.py
+++ ImageRecognition/image_generator.py
@@ -1,11 +1,21 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
 import tensorflow as tf
 
-train_dir = 'C:/Users/Isaac/Documents/Datasets/Generated'
+train_dir = 'C:\Users\Isaac\Documents\Datasets\Generated\horse-or-human\train'
 train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(train_dir, target_size=(300, 300), batch_size=128, class_mode='binary')
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(300, 300),
+                                                    batch_size=128,
+                                                    class_mode='binary')
 
+
+validation_generator = validation_datagen.flow_from_directory(train_dir,
+                                                    target_size=(300, 300),
+                                                    batch_size=128,
+                                                    class_mode='binary')
 
 
 model = tf.keras.models.Sequential([
@@ -15,7 +25,27 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
+
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc]'])
+
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=8,
+    epochs=15,
+    validation_data=validation_generator,
+    validation_steps=8,
+    verbose=2
+)
---
+++ ImageRecognition/image_generator_predict.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+
+
+model = keras.models.load_model("HorseOrHuman.h5")
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+
+    path = 'C:\Users\Isaac\Documents\Datasets\Generated\horse-or-human\validation' + fn
+    img = image.load_img(path, target_size=(300, 300))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    print(classes[0])
+    if(classes[0] > 0.5)
+        print(fn + " is a human")
+    else:
+        print(fn + " is a horse")
